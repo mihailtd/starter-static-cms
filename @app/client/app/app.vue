2f2faabfbffe0f4be0218fbec1d52f859bc4931c
Mihai Farcas <farcasmihai91@gmail.com>
2022-04-04 21:43:08 +0300

    feat: added TailwindCSS and GraphQL client

--- @app/client/app/app.vue
+++ @app/client/app/app.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+const { data, error } = useAsyncData("homepage", () => GqlHomepage());
+</script>
+
 <template>
-  <div>
-    <h1>Test</h1>
-  </div>
+  <h1 class="text-6xl text-blue-800">Hello From Nuxt + Strapi + TailwindCSS</h1>
+  <h2 class="text-4xl mt-20 text-blue-600" v-if="data">
+    {{ data.homepage?.data?.attributes?.title }}
+  </h2>
+  <p v-else-if="error">{{ error }}</p>
 </template>
